ad21774f3a08dc9db96a7986095e59e3f8660428
standagh <standa31415@gmail.com>
2019-05-23 12:18:05 +0200

    dev

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
-ansible-playbook  -b --become-method=sudo --ask-sudo-pass -i inventory_hadoop1 set_host_play.yml -vv
+
+[ -z "$1" ] && { echo "Missing param - inventory file"; exit 10;}
+
+ansible-playbook  -b --become-method=sudo -K -i "$1" set_host_play.yml -vv
 
