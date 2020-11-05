545e1a26f7f38eaf9c120442d2171e279cc426c7
standagh <standa31415@gmail.com>
2020-11-05 12:41:23 +0100

    update to ubuntu server 20.04

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 
 [ -z "$1" ] && { echo "Missing param - inventory file"; exit 10;}
 
-ansible-playbook  -b --become-method=sudo -K -i "$1" set_host_play.yml -vv
-
+ansible-playbook  -u root --private-key="$HOME/.ssh/privat/priv_rsa" -K -i "$1" set_host_play.yml -vvv
